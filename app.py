49cc1e6a72682030f9e7a9959d273dd49477370b
Madarapu Ravichandana <163497310+Chandana-madarapu@users.noreply.github.com>
2026-03-18 20:23:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,11 +5,14 @@ import nltk
 import os
 
 # ✅ Setup NLTK data directory
-nltk_data_path = os.path.join(os.getcwd(), "nltk_data")
-if not os.path.exists(nltk_data_path):
-    os.makedirs(nltk_data_path)
 
+nltk_data_path = os.path.join(os.getcwd(), "nltk_data")
 nltk.data.path.append(nltk_data_path)
+
+# Download required datasets
+nltk.download('punkt', download_dir=nltk_data_path)
+nltk.download('punkt_tab', download_dir=nltk_data_path)  # 🔥 IMPORTANT FIX
+nltk.download('stopwords', download_dir=nltk_data_path)
 
 # ✅ Download ONLY if missing
 try:
